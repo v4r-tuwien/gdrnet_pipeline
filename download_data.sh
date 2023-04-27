10bc3547b53e61bee3e7d69bccaa72eb6f31d5f9
Peter Hoenig <hpfreak1004@gmail.com>
2023-04-27 17:12:54 +0200

    updated download script

--- download_data.sh
+++ download_data.sh
@@ -1,8 +1,16 @@
-wget -O data.zip "https://owncloud.tuwien.ac.at/index.php/s/6GZSFUHRSMfUxxh/download"
-unzip data.zip
-cd verefine_data
-unzip data.zip
-mv data ../
-cd ../
-rm -rf verefine_data
-rm -rf data.zip
+wget -O yolov5_ycbv_weights.zip "https://owncloud.tuwien.ac.at/index.php/s/lbnwdUAR3uSpKOj/download"
+unzip yolov5_ycbv_weights.zip
+cp -r yolov5_ycbv_weights.pt ./src/yolov5/yolov5_ycbv_weights.pt
+rm -r yolov5_ycbv_weights.zip yolov5_ycbv_weights.pt
+
+wget -O gdrnpp_ycbv_weights.zip "https://owncloud.tuwien.ac.at/index.php/s/fkCygRgrV9C7zDH/download"
+unzip gdrnpp_ycbv_weights.zip
+mkdir -p ./src/gdrnpp/outputs
+cp -r gdrnpp_ycbv_weights.pth ./src/gdrnpp/outputs/gdrnpp_ycbv_weights.pth
+rm -r gdrnpp_ycbv_weights.zip gdrnpp_ycbv_weights.pth
+
+wget -O gdrnpp_ycbv_models.zip "https://owncloud.tuwien.ac.at/index.php/s/gUxThY2caSsvix2/download"
+unzip gdrnpp_ycbv_models.zip
+mkdir -p ./src/gdrnpp/datasets/BOP_DATASETS/ycbv/models
+cp -r models ./src/gdrnpp/datasets/BOP_DATASETS/ycbv/models
+rm -r gdrnpp_ycbv_models.zip models
